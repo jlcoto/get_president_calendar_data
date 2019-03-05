18db94ca1d3ee6381251c7e93d317fd38b4e51ce
jlcoto <jscoto@gmail.com>
2019-03-05 12:52:49 +0100

    Create first dag

---
+++ dags/init_dag.py
@@ -0,0 +1,26 @@
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from airflow.macros.data_lake_presidential_visitors import (
+    format_date_s3)
+
+from get_calendar_data import get_calendar_data
+
+default_args = {
+    'owner': 'me',
+    'start_date': dt.datetime(2017, 6, 1),
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=5),
+}
+
+with DAG('get_president_agenda',
+         default_args=default_args,
+         schedule_interval='0 * * * *',
+         ) as dag:
+
+    get_calendar_data = PythonOperator(task_id='get_calendar_data',
+                                       python_callable=get_calendar_data,
+                                       provide_context=True,
+                                       )
