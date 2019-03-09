8d940d7aa4276456f2b103c68e916542d010273b
jlcoto <jscoto@gmail.com>
2019-03-09 13:46:50 +0100

    Raise exception when request fails and get yesterday's data

--- dags/upload_visitors_data.py
+++ dags/upload_visitors_data.py
@@ -3,6 +3,7 @@ import requests
 import tempfile
 
 import boto3
+from airflow.exceptions import AirflowException
 
 import values
 from utils import format_date_scrape
@@ -15,16 +16,16 @@ client = session.client(
     aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
 )
 
-def upload_visitors_data(ts, ds, *args, **kwargs):
-    formatted_date = format_date_scrape(ds)
-
+def upload_visitors_data(prev_ds, *args, **kwargs):
+    formatted_date = format_date_scrape(prev_ds)
     VISITS_URL = values.VISITS_URL
     data = {'valorCaja1': formatted_date}
     response = requests.post(VISITS_URL, data=data)
-    data = response.content
-    key_name = f"{ds}_visits"
-
-    with tempfile.NamedTemporaryFile(mode='w+b') as temp:
-        temp.write(data)
-        temp.seek(0)
-        client.upload_file(temp.name, os.environ.get('AWS_DATA_LAKE_BUCKET'), key_name)
+    if response.ok:
+        key_name = f"{prev_ds}_visits"
+        with tempfile.NamedTemporaryFile(mode='w+b') as temp:
+            temp.write(response.content)
+            temp.seek(0)
+            client.upload_file(temp.name, os.environ.get('AWS_DATA_LAKE_BUCKET'), key_name)
+    else:
+        raise AirflowException("Request failed.")
