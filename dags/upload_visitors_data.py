2484f6dbac569ea90250fd91edbe617adead373a
jlcoto <jscoto@gmail.com>
2019-03-09 14:00:18 +0100

    Move S3 connections to utils

--- dags/upload_visitors_data.py
+++ dags/upload_visitors_data.py
@@ -2,19 +2,12 @@ import os
 import requests
 import tempfile
 
-import boto3
 from airflow.exceptions import AirflowException
 
 import values
-from utils import format_date_scrape
+from utils import format_date_scrape, get_s3_client
 
-session = boto3.Session(profile_name=os.environ.get('AWS_USER'))
-
-client = session.client(
-    's3',
-    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
-)
+client = get_s3_client()
 
 def upload_visitors_data(prev_ds, *args, **kwargs):
     formatted_date = format_date_scrape(prev_ds)
--- dags/utils.py
+++ dags/utils.py
@@ -1,5 +1,18 @@
 import datetime as dt
+import os
+
+import boto3
 
 def format_date_scrape(date):
     return dt.datetime.strptime(date, '%Y-%m-%d') \
              .strftime('%d/%m/%Y')
+
+def get_s3_client():
+    session = boto3.Session(profile_name=os.environ.get('AWS_USER'))
+
+    client = session.client(
+        's3',
+        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
+    )
+    return client
